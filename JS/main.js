c5ea806154c64c719dcc50d89995bcc3c69b1511
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-08 17:45:15 +0000

    started

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+    let state = {
+    symbols: [["&", "@", "#", "$", "%", "^", "&", "*", "("]],
+    currentPage: 0,
+    pages: [
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+        {
+            headerText: "I can read your mind",
+            paragraphText: "",
+            nextButtonText: "",
+        },
+    ]
+}
